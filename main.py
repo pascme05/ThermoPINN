aa83726fdf220d3191dfe7f913b07e5a6957cc2a
Pascal Schirmer <66561268+pascme05@users.noreply.github.com>
2025-10-18 17:11:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-# ----------------------------------------------------
-# pinn_thermal_lstm_csv.py
-# Author: Pascal Schirmer
-# Date: 2025-10-12
-# Description:
-#   Physics-informed neural network (PINN) using an LSTM model
-#   for thermal prediction from CSV input data. The script
-#   performs RC identification, model training, and evaluation
-#   with optional visualization.
-# ----------------------------------------------------
+#######################################################################################################################
+#######################################################################################################################
+# Title:        ThermoPINN
+# Topic:        Physics Informed Neural Network (PINN) for thermal modeling
+# File:         main
+# Date:         17.10.2025
+# Author:       Dr. Pascal A. Schirmer
+# Version:      V.0.1
+# Copyright:    Pascal Schirmer
+#######################################################################################################################
+#######################################################################################################################
 
+
+#######################################################################################################################
+# Import external libs
+#######################################################################################################################
 import os
 import torch.optim as optim
 import matplotlib.pyplot as plt
@@ -18,10 +23,13 @@ from sklearn.pipeline import make_pipeline
 from src.model import *
 
 
-# ----------------------------------------------------
+#######################################################################################################################
 # Main workflow
-# ----------------------------------------------------
+#######################################################################################################################
 def main():
+    # ==============================================================================
+    # Config / Parameters
+    # ==============================================================================
     # -------------------------------
     # Config / Parameters
     # -------------------------------
@@ -31,12 +39,17 @@ def main():
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     DATA_PATH = os.path.join(BASE_DIR, "data", "motor_temp.csv")
+    MDL_NAME = "mdl/mdl_best_pinn.pt"
 
+    # -------------------------------
     # Fixed Thermal parameters
+    # -------------------------------
     Rth = []                                                                                                             # Thermal resistance foster network [K/W]
     Cth = []                                                                                                             # Thermal capacitance foster network [Ws/K]
 
+    # -------------------------------
     # Motor parameters
+    # -------------------------------
     Rs = 14.1e-3                                                                                                         # Stator resistance [Ohm]
     alpha = 0.00393                                                                                                      # Temperature coefficient of resistance [1/°C]
     Tref = 20                                                                                                            # Reference temperature for Rs [°C]
@@ -44,7 +57,9 @@ def main():
     beta_1 = 0.315                                                                                                       # Parameter for frequency losses (linear)
     beta_2 = 0.616                                                                                                       # Parameter for frequency losses (quadratic)
 
+    # -------------------------------
     # Training hyperparameters
+    # -------------------------------
     seq_len = 1300                                                                                                        # Sequence length (timesteps per training sample)
     stride = 50                                                                                                          # Step size between training sequences
     batch_size = 32                                                                                                      # Batch size for training
@@ -53,10 +68,15 @@ def main():
     lr = 1.67e-3                                                                                                            # Learning rate for optimizer
     epochs = 100                                                                                                          # Maximum number of training epochs
     lambda_phys = 0.02                                                                                                    # Weight for physics-informed loss term
-    lambda_init = 0.000                                                                                                    # Weight for initial condition loss (currently unused)
+    lambda_init = 0.50                                                                                                   # Weight for initial condition loss (currently unused)
     patience = 10                                                                                                        # Early stopping patience (epochs without improvement)
 
+    # ==============================================================================
+    # Data
+    # ==============================================================================
+    # -------------------------------
     # Dataset split IDs
+    # -------------------------------
     test_ids = [60, 62, 74]                                                                                              # IDs used for test set evaluation
     val_ids = [10, 48, 63]                                                                                               # IDs used for validation set selection
 
@@ -65,6 +85,9 @@ def main():
     # -------------------------------
     df = pd.read_csv(DATA_PATH)
 
+    # ==============================================================================
+    # RC Identification
+    # ==============================================================================
     # -------------------------------
     # RC Identification
     # -------------------------------
@@ -106,19 +129,18 @@ def main():
         C_hat = Cth[0]
         print(f"Defined Average: R_th = {R_hat:.4f} K/W, C_th = {C_hat:.2f} J/K")
 
+    # ==============================================================================
+    # Pre-processing
+    # ==============================================================================
     # -------------------------------
     # Feature preparation
     # -------------------------------
-    # Calc features
     f1 = (1 + alpha * (df["Tc"] - Tref))
     f2 = 1 + beta_1 * (df["Wm"] / n_max) + beta_2 * (df["Wm"] / n_max) ** 2
     df["Pv_s"] = 3 * Rs * (df["Is"] / np.sqrt(2)) ** 2 * f1 * f2
     df["Sel"] = 3/2 * df["Is"] * df["Us"]
     df["SelI"] = df["Sel"] * df["Is"]
     df["SelW"] = df["Sel"] * df["Wm"]
-
-    # Remove features
-    feature_cols = [c for c in df.columns if c not in ["id", "time", "time_id", "T0", "Tsw", "Tst", "Tso", "Trm"]]
 
     # -------------------------------
     # Data Splitting
@@ -131,6 +153,9 @@ def main():
     # -------------------------------
     # Normalize and Scale
     # -------------------------------
+    # Features
+    feature_cols = [c for c in df.columns if c not in ["id", "time", "time_id", "T0", "Tsw", "Tst", "Tso", "Trm"]]
+
     # Norm values
     X_mean, X_std = df_train[feature_cols].mean(), df_train[feature_cols].std() + 1e-8
     T_min, T_max = df_train["Tsw"].min(), df_train["Tsw"].max()
@@ -147,9 +172,6 @@ def main():
     P_train *= (1 + alpha * (df_train["Tsw"] - Tref)) * (1 + beta_1 * (df_train["Wm"] / n_max) + beta_2 * (df_train["Wm"] / n_max) ** 2)
     P_val *= (1 + alpha * (df_val["Tsw"] - Tref)) * (1 + beta_1 * (df_val["Wm"] / n_max) + beta_2 * (df_val["Wm"] / n_max) ** 2)
 
-    #P_train = df_train["Pv_s"]
-    #P_val = df_train["Pv_s"]
-
     # -------------------------------
     # DataLoaders
     # -------------------------------
@@ -160,6 +182,9 @@ def main():
                                 df_val["T0"].values, seq_len, stride, batch_size, DEVICE,
                                 df[df["id"].isin(test_ids)]["id"].to_numpy(), shuffle=False)
 
+    # ==============================================================================
+    # Training
+    # ==============================================================================
     # -------------------------------
     # Model setup
     # -------------------------------
@@ -220,19 +245,27 @@ def main():
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
                 patience_counter = 0
-                torch.save(model.state_dict(), "mdl/mdl_best_pinn.pt")
+                torch.save(model.state_dict(), MDL_NAME)
             else:
                 patience_counter += 1
                 if patience_counter >= patience:
                     print("Early stopping triggered.")
                     break
 
+    # ==============================================================================
+    # Testing
+    # ==============================================================================
     # -------------------------------
-    # Test evaluation
+    # Setup
     # -------------------------------
-    model.load_state_dict(torch.load("mdl/mdl_best_pinn.pt"))
+    model.load_state_dict(torch.load(MDL_NAME))
     model.eval()
+    T_pred_all = np.empty((0,), float)
+    T_true_all = np.empty((0,), float)
 
+    # -------------------------------
+    # Loop
+    # -------------------------------
     for sid in test_ids:
         print(f"\n🔹 Evaluating Test Session ID: {sid}")
 
@@ -259,6 +292,10 @@ def main():
             T_true_nn = T_test * (T_max - T_min) + T_min
             T_pred_nn_phys = T_pred_nn * (T_max - T_min) + T_min
 
+            # Append Total
+            T_pred_all = np.append(T_pred_all, T_pred_nn_phys, axis=0)
+            T_true_all = np.append(T_true_all, T_true_nn, axis=0)
+
             # Compute errors
             err_rc = T_pred_rc - T_true_nn
             err_nn = T_pred_nn_phys - T_true_nn
@@ -278,37 +315,58 @@ def main():
             print(f"NN MAE (°C): {mae_test_nn:.2f}")
             print(f"NN MAX (°C): {max_test_nn:.2f}")
 
-            # -------------------------------
             # Plotting per session
-            # -------------------------------
             if ENABLE_PLOTS:
                 time = np.linspace(0, (len(err_rc) - 1) / 60, len(err_rc))  # time in minutes
-                fig, axs = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
+                fig, axs = plt.subplots(3, 1, figsize=(10, 6), sharex=True)
                 fig.suptitle(f"Session ID {sid} – Model Comparison", fontsize=12, fontweight="bold")
 
-                # ▪️ Top: Temperatures
-                axs[0].plot(time, T_true_nn, label="Measured", color="black", linewidth=2)
-                axs[0].plot(time, T_pred_rc, label="Pred RC", linewidth=1.8)
-                axs[0].plot(time, T_pred_nn_phys, label="Pred NN", linewidth=1.8)
-                axs[0].set_ylabel("Temperature [°C]")
+                # ▪️ Top: Current Voltage
+                axs[0].plot(time, df_session["Is"].values, label="Current", linewidth=2)
+                axs[0].plot(time, df_session["Us"].values, label="Voltage", linewidth=2)
+                axs[0].set_ylabel("Current [A] / Voltage [V]")
                 axs[0].legend(loc="best")
                 axs[0].grid(True, linestyle="--", linewidth=0.6)
 
-                # ▪️ Bottom: Errors
-                axs[1].plot(time, err_rc, label="RC Error")
-                axs[1].plot(time, err_nn, label="NN Error")
-                axs[1].axhline(0, color="black", linewidth=1)
-                axs[1].set_xlabel("Time [min]")
-                axs[1].set_ylabel("Error [°C]")
+                # ▪️ Mid: Temperatures
+                axs[1].plot(time, T_true_nn, label="Measured", color="black", linewidth=2)
+                axs[1].plot(time, T_pred_rc, label="Pred RC", linewidth=1.8)
+                axs[1].plot(time, T_pred_nn_phys, label="Pred NN", linewidth=1.8)
+                axs[1].plot(time, df_session["Tc"].values, label="Coolant", linewidth=1.8)
+                axs[1].set_ylabel("Temperature [°C]")
                 axs[1].legend(loc="best")
                 axs[1].grid(True, linestyle="--", linewidth=0.6)
 
+                # ▪️ Bottom: Errors
+                axs[2].plot(time, err_rc, label="RC Error")
+                axs[2].plot(time, err_nn, label="NN Error")
+                axs[2].axhline(0, color="black", linewidth=1)
+                axs[2].set_xlabel("Time [min]")
+                axs[2].set_ylabel("Error [°C]")
+                axs[2].legend(loc="best")
+                axs[2].grid(True, linestyle="--", linewidth=0.6)
+
                 plt.tight_layout()
+
+    # Total Error
+    err_nn_all = T_pred_all - T_true_all
+
+    # Error Metrics
+    mse_test_nn_all = np.mean(err_nn_all ** 2)
+    mae_test_nn_all = np.mean(np.abs(err_nn_all))
+    max_test_nn_all = np.max(np.abs(err_nn_all))
+
+    print(f"\n🔹 Evaluating Test Total")
+    print(f"NN MSE (°C): {mse_test_nn_all:.2f}")
+    print(f"NN MAE (°C): {mae_test_nn_all:.2f}")
+    print(f"NN MAX (°C): {max_test_nn_all:.2f}")
+
+    # Show plot
     plt.show()
 
 
-# ----------------------------------------------------
+#######################################################################################################################
 # Entry point
-# ----------------------------------------------------
+#######################################################################################################################
 if __name__ == "__main__":
     main()
